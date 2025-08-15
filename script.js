6ca4f481ce950d5e8b18375d24f1c015867d3764
Paul Li <paulkay19@gmail.com>
2025-08-15 12:10:02 +0800

    Add a throw error to divide function

--- script.js
+++ script.js
@@ -1,46 +1,29 @@
 function add(sum) {
-  return sum.reduce(function(acc, value) {
+  return sum.reduce(function (acc, value) {
     return acc + value;
-  }, 0)
-
+  }, 0);
 }
 
 function subtract(difference) {
-    return difference.reduce(function(acc, value) {
-        return acc - value;
-    });
+  return difference.reduce(function (acc, value) {
+    return acc - value;
+  });
 }
 
-
 function multiply(product) {
-    return product.reduce(function(acc, value) {
-        return acc * value;
-    }, 1);
+  return product.reduce(function (acc, value) {
+    return acc * value;
+  }, 1);
 }
 
 function divide(quotient) {
-    return quotient.reduce(function(acc, value) {
-        if (value === 0) {
-            throw new Error("Cannot divide by zero.")
-        }
-        return acc / value;
-    });
-
+  return quotient.reduce(function (acc, value) {
+    if (value === 0) {
+      throw new Error("Cannot divide by zero.");
+    }
+    if (quotient.length === 0) {
+      throw new Error("Empty input");
+    }
+    return acc / value;
+  });
 }
-
-
-let initialNumber = "";
-
-let operator = "";
-
-let secondNumber = "";
-
-
-
-
-
-
-
-
-
-
